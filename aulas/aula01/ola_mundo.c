ffbd0f285f9568be83e7c1e046db2bf2318a242c
unknown <2612130007@ACADEMICO.IESB.BR>
2026-03-11 10:58:37 -0300

    adiciona codigos da aula02

--- aulas/aula01/ola_mundo.c
+++ aulas/aula01/ola_mundo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("beijo");
+    printf("oiioi");
      return 0;
 }
---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+// inclusao de biobliotecas de linguagem
+#include <stdio.h>
+
+int main() {
+    // 1. Entrada
+    // 2. Processamento
+    // 3. Saida
+    printf("resident evil eh bao demais\n");
+    printf("Programar em C eh top!\n");
+    printf("%i\n", 10);
+    printf("%f\n",10.3455);
+    return 0;
+}  
+    
