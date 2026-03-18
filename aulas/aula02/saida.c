6d4c973bf3eadc152a7a03e45de9d54b34f416cb
unknown <2612130007@ACADEMICO.IESB.BR>
2026-03-18 10:59:26 -0300

    codigos da aula02

--- aulas/aula02/saida.c
+++ aulas/aula02/saida.c
@@ -8,7 +8,6 @@ int main() {
     printf("resident evil eh bao demais\n");
     printf("Programar em C eh top!\n");
     printf("%i\n", 10);
-    printf("%f\n",10.3455);
+    printf("%f\n", 10.3455);
     return 0;
 }  
-    
